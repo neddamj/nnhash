6b47c5b38fff3dd396baf02f8bef477192634948
Jordan Madden <neddamj@Jordans-Air-2.attlocal.net>
2022-12-27 23:09:07 -0500

    untargeted attack works crudely. needs further testing

--- src/simba_attack.py
+++ src/simba_attack.py
@@ -1,17 +1,23 @@
 from utils import compute_hash, sample_pixel, save_img
 from PIL import Image
 import numpy as np
+import copy
 import cv2
 
-def get_hash_of_preturbed_imgs(pixel, H, W, path, img, stepsize):
+def get_hash_of_preturbed_imgs(pixel, H, W, path, add_img, sub_img, stepsize):
     _, _, filename, filetype = path.split('.')
-    add_img, sub_img = np.copy(img), np.copy(img)
     # Add value to the pixel and get the hash 
-    add_img[H][W] = pixel + stepsize
+    if pixel + stepsize <= 255.0:
+        add_img[H][W] = pixel + stepsize
+    else:
+        add_img[H][W] == 255.0
     save_img(f'../{filename}_add.{filetype}', add_img)
     add_hash = compute_hash(f'../{filename}_add.{filetype}')
     # Subtract value from the pixel and get the hash 
-    sub_img[H][W] = pixel - stepsize
+    if pixel - stepsize >= 0.0:
+        sub_img[H][W] = pixel - stepsize
+    else:
+        sub_img[H][W] = 0.0
     save_img(f'../{filename}_sub.{filetype}', sub_img)
     sub_hash = compute_hash(f'../{filename}_sub.{filetype}')
     return (add_img, add_hash, sub_img, sub_hash)
@@ -19,26 +25,31 @@ def get_hash_of_preturbed_imgs(pixel, H, W, path, img, stepsize):
 if __name__ == "__main__":
     img_path = '../images/01.jpg'
     _, _, filename, filetype = img_path.split('.')
+    # Initialize images
     img = np.array(Image.open(img_path)).astype(np.float32)
+    add_img, sub_img = copy.deepcopy(img), copy.deepcopy(img)
     init_hash = compute_hash(img_path)
-    stepsize, counter = 1.0, 0
+    stepsize, counter, i = 200.0, 0, 0
     # Store the initial hash
     hashes = []
     hashes.append(init_hash)
-    for i in range(25000):
+    while True:
+        i += 1
         (pixel, Y, X) = sample_pixel(img)
-        (add_img, additive_hash, sub_img, subtractive_hash) = get_hash_of_preturbed_imgs(pixel, Y, X, img_path, img, stepsize)
-        print(f'Iteration: {i+1}\tAdd Hash: {hex(additive_hash)}\tSub Hash: {hex(subtractive_hash)}')
+        (add_img, additive_hash, sub_img, subtractive_hash) = get_hash_of_preturbed_imgs(pixel, Y, X, img_path, add_img, sub_img, stepsize)
+        print(f'Iteration: {i} \tAdd Hash: {hex(additive_hash)} \tSub Hash: {hex(subtractive_hash)}')
         if abs(init_hash-additive_hash) > abs(init_hash-subtractive_hash):
             # replace original image by additive image and store the new hash
             counter += 1
             hashes.append(additive_hash)
-            print('[INFO] Saving Added Image')
+            print(f'[INFO] Saving Added Image after {i} iterations')
             save_img(f'../{filename}_new.{filetype}', add_img)
+            break
         elif abs(init_hash-additive_hash) < abs(init_hash-subtractive_hash):
             # replace original image by subtractive image and store the new hash
             counter += 1
             hashes.append(subtractive_hash)
-            print('[INFO] Saving Subtracted Image')
+            print(f'[INFO] Saving Subtracted Image after {i} iterations')
             save_img(f'../{filename}_new.{filetype}', sub_img)
-    print(f'The hash was changes {counter} times')
+            break
+    print(f'The hash was changed {counter} times')
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import numpy as np
 import subprocess
-import torch
 import cv2
 
 def compute_hash(image_path, return_type='hex'):
@@ -12,18 +11,13 @@ def compute_hash(image_path, return_type='hex'):
 
 def sample_pixel(img):
     (Y, X) = img.shape[0], img.shape[1]
-    (Y, X) = (int(Y*torch.rand(1)), int(X*torch.rand(1)))
+    (Y, X) = (int(Y*np.random.random(1)), int(X*np.random.random(1)))
     pixel = img[Y][X]
-    # if pixel is 0 or 255 choose a new pixel
-    if int(pixel) == 255 or int(pixel) == 0:
-        (Y, X) = (int(Y*torch.rand(1)), int(X*torch.rand(1)))
-        pixel = img[Y][X]
     return (pixel, Y, X)
  
 def save_img(save_path, img):
     img = Image.fromarray(np.uint8(img))
     img.save(save_path)
-    #cv2.imwrite(save_path, img)
 
 def load_img(img_path):
     return np.array(Image.open(img_path)).astype(np.float32)
