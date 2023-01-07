618b256eba4d1df29e8af073679bb45f04e15b4b
Jordan Madden <neddamj@Jordans-Air-2.attlocal.net>
2023-01-07 14:22:04 -0500

    Calculate ASR and Avg Distortion for batch

--- src/simba_attack.py
+++ src/simba_attack.py
@@ -49,7 +49,7 @@ def get_hash_of_batch(pixels, H, W, C, paths, add_imgs, sub_imgs, stepsize, batc
     sub_hashs.append(compute_hash(sub_paths, batch=batch))
     return (add_ims, add_hashs, sub_ims, sub_hashs)
 
-def simba_attack_image(img_path, eps):
+def simba_attack_image(img_path, eps, max_steps=5000):
     filename, filetype = img_path.split('.')
     # Initialize images
     img = load_img(img_path)
@@ -57,7 +57,7 @@ def simba_attack_image(img_path, eps):
     init_hash = compute_hash(img_path)
     i = 0
     stepsize = int(eps*255.0)
-    while True:
+    while i < max_steps:
         i += 1
         (pixel, Y, X, Z) = sample_pixel(img)
         (add_img, additive_hash, sub_img, subtractive_hash) = get_hash_of_preturbed_imgs(pixel, 
@@ -74,7 +74,7 @@ def simba_attack_image(img_path, eps):
             dist = np.linalg.norm((add_img-img)/255)
             # replace original image by additive image and store the new hash
             logger.info(f'Saving {filename}_add.{filetype} after {i} iterations')
-            logger.info(f'L2 Distortion: {dist} units')
+            logger.info(f'L2 Distortion: {dist:.2f} units')
             logger.info(f'Initial Hash: {hex(init_hash)}\tNew Hash: {hex(additive_hash)}')
             save_img(f'{filename}_new.{filetype}', add_img)
             break
@@ -83,22 +83,23 @@ def simba_attack_image(img_path, eps):
             dist = np.linalg.norm(sub_img-img/255)
             # replace original image by subtractive image and store the new hash
             logger.info(f'Saving {filename}_new.{filetype} after {i} iterations')
-            logger.info(f'L2 Distortion: {dist} units')
+            logger.info(f'L2 Distortion: {dist:.2f} units')
             logger.info(f'Initial Hash: {hex(init_hash)}\tNew Hash: {hex(subtractive_hash)}')
             save_img(f'{filename}_new.{filetype}', sub_img)
             break
     print(f'\nThe distortion to the original image is {dist:.2f} units')
 
-def simba_attack_batch(folder_path, eps, batch=True):
+def simba_attack_batch(folder_path, eps, max_steps=5000, batch=True):
     img_paths = load_img_paths(folder_path)
     resize_imgs(img_paths, batch=batch)
     imgs = load_img(img_paths, batch=batch)
     add_imgs, sub_imgs = copy.deepcopy(imgs), copy.deepcopy(imgs)
     init_hashes = compute_hash(img_paths, batch=batch)
-    i = 0
+    counter, i = 0, 0
+    total_imgs = len(img_paths)
     stepsize = int(eps*255.0)
-    hashes = []
-    while True:
+    hashes, distortion = [], []
+    while i < max_steps:
         if len(img_paths) == 0:
             break
         i += 1
@@ -116,15 +117,17 @@ def simba_attack_batch(folder_path, eps, batch=True):
         nz_subs = np.nonzero(init_hashes-subtractive_hashes[0])[0]    # Get the indices of the nonzero subtractive hashes
         print(f'Iteration: {i}')
         for index in nz_adds:
+            counter += 1
             # calculate l2 distortion 
             dist = np.linalg.norm((add_imgs[index]-imgs[index])/255)
             filename, filetype = img_paths[index].split('.')
-            logger.info(f'Saving {filename}_add.{filetype} after {i+1} iterations')
-            logger.info(f'L2 Distortion: {dist} units')
+            logger.info(f'Saving "{filename}_add.{filetype}" after {i+1} iterations')
+            logger.info(f'L2 Distortion: {dist:.2f} units')
             logger.info(f'Old Hash: {init_hashes[index]} \tNew Hash: {additive_hashes[0][index]}\n')
             save_img(f'{filename}_new.{filetype}', add_imgs[index])
-            # Save the new hashes
+            # Save the new hashes and distortions
             hashes.append(additive_hashes[0][index])
+            distortion.append(dist)
             # Remove the hashed image from the caches
             init_hashes = np.delete(init_hashes, index)
             additive_hashes = np.delete(additive_hashes, index)
@@ -133,15 +136,17 @@ def simba_attack_batch(folder_path, eps, batch=True):
             add_imgs.pop(index)
             sub_imgs.pop(index)
         for index in nz_subs:
+            counter += 1
             # calculate l2 distortion 
             dist = np.linalg.norm((sub_imgs[index]-imgs[index])/255)
             filename, filetype = img_paths[index].split('.')
-            logger.info(f'Saving {filename}_sub.{filetype} after {i+1} iterations')
-            logger.info(f'L2 Distortion: {dist} units')
+            logger.info(f'Saving "{filename}_sub.{filetype}" after {i+1} iterations')
+            logger.info(f'L2 Distortion: {dist:.2f} units')
             logger.info(f'Old Hash: {init_hashes[index]} \tNew Hash: {subtractive_hashes[0][index]}\n')
             save_img(f'{filename}_new.{filetype}', sub_imgs[index])
-            # Save the new hashes
+            # Save the new hashes and distortions
             hashes.append(subtractive_hashes[0][index])
+            distortion.append(dist)
             # Remove the hashed image from the caches
             init_hashes = np.delete(init_hashes, index)
             additive_hashes = np.delete(additive_hashes, index)
@@ -149,7 +154,8 @@ def simba_attack_batch(folder_path, eps, batch=True):
             img_paths.pop(index)
             add_imgs.pop(index)
             sub_imgs.pop(index)
-        
+    logging.info(f'Total Steps: {i}\t Attack Success Rate: {100*(counter/total_imgs):.2f}%\t \
+        Average Distortion: {sum(distortion)/len(distortion):.2f}')
         
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -177,10 +183,11 @@ if __name__ == "__main__":
 
     # Hyperparams
     epsilon = 0.9
+    logger.info(f'Epsilon: {epsilon}\n')
     if not batch:
         _, _, path, filetype = img_path.split('.')
         img_path = path.split('/')
         img_path = f'{folder_path}{img_path[2]}.{filetype}'
-        simba_attack_image(img_path, eps=epsilon)
+        simba_attack_image(img_path, eps=epsilon, max_steps=10000)
     else:
-        simba_attack_batch(folder_path, eps=epsilon)
+        simba_attack_batch(folder_path, eps=epsilon, max_steps=10000)
