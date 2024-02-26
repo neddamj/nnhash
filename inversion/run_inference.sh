67bf9bebda7efef0c9956666d210dd5349bd944d
Jordan Madden <jordanmadden285@gmail.com>
2024-02-26 12:24:21 -0500

    scripts to automate training and inference

---
+++ inversion/run_inference.sh
@@ -0,0 +1,2 @@
+python data.py --dataset 'mnist' --train_len 10000 --val_len 100 --skip True
+python generate.py --rgb False --display False
---
+++ inversion/run_training.sh
@@ -0,0 +1,2 @@
+python data.py --dataset 'mnist' --train_len 10000 --val_len 100 --skip False
+python train.py --rgb True --epochs 50 --batch_size 50 --learning_rate 0.0005
