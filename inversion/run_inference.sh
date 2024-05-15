ac71833ddb5fa3afc3a24d57104af3d2bcaee600
Jordan Madden <jordanmadden285@gmail.com>
2024-05-15 12:25:38 -0400

    update

--- inversion/run_inference.sh
+++ inversion/run_inference.sh
@@ -1,4 +1,4 @@
 echo '[INFO] Beginning data loading...'
-python data.py --dataset 'celeba' --train_len 200 --val_len 20 --skip 0
+python data.py --dataset 'celeba' --train_len 200 --val_len 20 --skip 1
 echo '[INFO] Data loading complete, running inference now...'
-python generate.py --rgb 1 --display 1 --hash_func 'pdq' --path /your/file/path/here/
+python generate.py --rgb 1 --display 1 --hash_func 'photodnapdq' --path /path/to/model
