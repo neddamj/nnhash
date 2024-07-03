abb306243809f82167f2ea7b3291dc8aa7e41e51
neddamj <jordanmadden285@gmail.com>
2024-07-02 22:10:11 -0400

    add new parameter

--- inversion/run_inference.sh
+++ inversion/run_inference.sh
@@ -1,4 +1,4 @@
 echo '[INFO] Beginning data loading...'
 python data.py --dataset 'celeba' --train_len 20000 --val_len 100 --hash_func 'neuralhash' --skip 1
 echo '[INFO] Data loading complete, running inference now...'
-python generate.py --rgb 1 --display 0 --hash_func 'neuralhash' --perturb 0.0 --path /path/to/model
+python generate.py --rgb 1 --display 1 --hash_func 'photodna' --dataset 'stl10' --perturb 0.0 --path /path/to/inversion/model
